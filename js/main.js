d0628246a0338b2fbfdf2635d9a7921b198e54fa
piaocreative <johnpiao171019@gmail.com>
2021-09-11 04:22:42 +0800

    demo53-0911

---
+++ js/main.js
@@ -0,0 +1,132 @@
+$( document ).ready( function() {
+    
+    $('.products').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+      $('.featured-products').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows:false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+      $('.teams').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+
+      $(".product-prev").click(function(e) {
+        e.preventDefault();
+        $('.products').slick('slickPrev');
+      })
+      
+      $(".team-next").click(function(e) {
+        e.preventDefault();
+        $('.teams').slick('slickNext');
+      })
+
+      $(".team-prev").click(function(e) {
+        e.preventDefault();
+        $('.products').slick('slickPrev');
+      })
+      
+      $(".product-next").click(function(e) {
+        e.preventDefault();
+        $('.teams').slick('slickNext');
+      })
+
+      $(".featured-prev").click(function(e) {
+        e.preventDefault();
+        $('.featured-products').slick('slickPrev');
+      })
+      
+      $(".featured-next").click(function(e) {
+        e.preventDefault();
+        $('.featured-products').slick('slickNext');
+      })
+
+} );
