d00efd613647d1667cce8a63e3c31efa9d5b3651
piaocreative <johnpiao171019@gmail.com>
2021-09-27 14:09:51 +0800

    demo53-0926

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 $( document ).ready( function() {
+
+  $('.logos').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
     
     $('.products').slick({
         dots: false,
